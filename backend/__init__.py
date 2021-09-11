e18118488db984b536dfa233103d4ac57bedaf36
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-09-11 16:19:31 +0200

    add some code.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,2 +1,20 @@
-def get_files():
+import os
+
+HOME = os.path.expanduser("~")
+
+
+def get_files(directory=HOME):
+    """
+    files = {
+        "filename":
+    }
+    """
+    files = {}
+    for file in os.walk(directory):
+        print(file)
+
     return
+
+
+if __name__ == "__main__":
+    get_files("../testdir")
