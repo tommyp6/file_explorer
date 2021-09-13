0e9d42ce3790daa12c0412f2f7bcf1966e522eb3
Tommy <43862943+tommyp6@users.noreply.github.com>
2021-09-13 17:48:12 +0200

    make some changes

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,8 +4,8 @@ from pathlib import Path as _Path
 HOME = os.path.expanduser("~")
 
 
-class File:
-    def __init__(self, path, size, last_modified, owner, group):
+class Path:
+    def __init__(self, path, size=None, last_modified=None, owner=None, group=None):
         self.path = path
         self.name = os.path.basename(path)
         self.size = size
@@ -13,73 +13,53 @@ class File:
         self.owner = owner
         self.group = group
 
+        if self.size is None:
+            self.size = os.path.getsize(self.path)
+
+        if self.last_modified is None:
+            self.last_modified = os.path.getctime(self.path)
+
+        if self.owner is None:
+            self.owner = _Path(self.path).owner()
+
+        if self.group is None:
+            self.group = _Path(self.path).group()
+
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}> {self.size}\t{self.owner}:{self.group}\t{self.last_modified}\t{self.name}"
-
-
-class Path:
-    def __init__(self, path):
-        self.path = path
-        self.folders = []
-        self.files = []
+        return f"<{self.type()}> {self.size}\t{self.owner}:{self.group}\t{self.last_modified}\t{self.name}"
 
     def __iter__(self):
-        self.items()
-
-        for folder in self.folders:
-            yield folder
-
-        for file in self.files:
-            yield file
-
-    def __str__(self):
-        return self.path
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}> {self.path}"
-
-    def items(self):
-        for item in os.listdir(self.path):
-            item = os.path.join(self.path, item)
-            if os.path.isfile(item):
+        if not os.path.isfile(self.path):
+            for item in os.listdir(self.path):
+                item = os.path.join(self.path, item)
                 _p = _Path(item)
-                file = File(
+                base = Path(
                     item,
                     os.path.getsize(item),
                     os.path.getctime(item),
                     _p.owner(),
                     _p.group(),
                 )
-                self.files.append(file)
+                yield base
 
-            elif os.path.isdir(item):
-                folder = Folder(os.path.join(self.path, item), item)
-                self.folders.append(folder)
+    def type(self):
+        if os.path.isfile(self.path):
+            return "File"
 
-        return [*self.folders, *self.files]
-
-
-class Folder(Path):
-    def __init__(self, path, name):
-        super().__init__(path)
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}> {self.name}"
+        if os.path.isdir(self.path):
+            return "Folder"
 
 
 if __name__ == "__main__":
     pwd = os.path.dirname(os.path.abspath(__file__))
     p = Path(os.path.join(pwd, "testdir"))
 
-    for item in p.items():
-        print(repr(item))
-        if isinstance(item, Folder):
-            for i in item:
-                print("\t", repr(i))
+    # Go 1 level deep.
+    print(repr(p))
+    for item in p:
+        print(f"\t{repr(item)}")
+        for i in item:
+            print(f"\t\t{repr(i)}")
