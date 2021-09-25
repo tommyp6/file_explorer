9e07b88fa8ed626c02db8a5c80a159267e2e879d
petmas2008 <55663672+petmas2008@users.noreply.github.com>
2021-09-25 15:26:14 +0100

    test

--- backend/handle_search.py
+++ backend/handle_search.py
@@ -1,12 +1,22 @@
-from . import get_files
-from frontend import get_search
+# from . import get_files
+# from frontend import get_search
 import os
 
 
-def find_files():
-    files = get_files()
-    user_search = get_search()
+def find_files(files, user_search):
+    # files = get_files()
+    # user_search = get_search()
     files_found = []
     for file in files[user_search]:
         if file == os.path.basename(user_search):
             files_found.append(file)
+
+    files_found = [file for file in files[user_search] if file == os.path.basename(user_search)]
+
+    return files_found
+
+
+if __name__ == "__main__":
+    files = {"hello.txt":[], "world.txt":[]}
+    user_search = "hello.txt"
+    print(find_files(files, user_search))
